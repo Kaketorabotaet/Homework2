91479db65adaa8e45c06e5141a49c716ea755c3a
Kaketorabotaet <danil1595@mail.ru>
2022-06-13 16:03:14 +0500

    справился с 4 задачей

---
+++ src/lession4/animalName.java
@@ -0,0 +1,17 @@
+package lession4;
+
+public class animalName {
+    String nameAnimal;
+
+    public void setNameAnimal(String nameAnimal) {
+        this.nameAnimal = nameAnimal;
+    }
+
+    public String getNameAnimal() {
+        return nameAnimal;
+    }
+
+    public animalName(String nameAnimal) {
+        this.nameAnimal = nameAnimal;
+    }
+}
---
+++ src/lession4/animalSpeed.java
@@ -0,0 +1,17 @@
+package lession4;
+
+public class animalSpeed {
+    double speedAnimal;
+
+    public void setSpeedAnimal(double speedAnimal) {
+        this.speedAnimal = speedAnimal;
+    }
+
+    public double getSpeedAnimal() {
+        return speedAnimal;
+    }
+
+    public animalSpeed(double speedAnimal) {
+        this.speedAnimal = speedAnimal;
+    }
+}
