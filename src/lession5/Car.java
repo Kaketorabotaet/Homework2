6d8b9b2311cc297e51abfb6da2359f14ce4bcca6
Kaketorabotaet <danil1595@mail.ru>
2022-06-21 19:33:28 +0500

    class car

---
+++ src/lession5/Car.java
@@ -0,0 +1,13 @@
+package lession5;
+
+public abstract class Car {
+    public void rides() {
+        System.out.println("Машина начинает движение");
+    }
+    public void stop() {
+        System.out.println("Машина остановилась");
+    }
+    public void light() {
+        System.out.println("Машина включила фары");
+    }
+}
